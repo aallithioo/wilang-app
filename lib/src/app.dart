1f7c935427f224bb3ff11058d52dde9f7f50b7ed
Thio Alli <connect.thioalli@gmail.com>
2022-02-22 09:01:04 +0700

    refactor : app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'app/routes/route.dart';
+import 'app/themes/theme.dart';
+
+import 'view/setting/child/about/about_screen.dart';
+import 'view/setting/child/hall_of_fame/hall_of_fame_screen.dart';
+import 'view/setting/child/privacy_policy/privacy_policy_screen.dart';
+import 'view/setting/child/release_notes/release_notes_screen.dart';
+import 'view/setting/child/report_bug/report_bug_screen.dart';
+import 'view/setting/child/support/support_screen.dart';
+import 'view/setting/child/terms/terms_screen.dart';
+import 'view/setting/child/theme/theme_screen.dart';
+import 'view/setting/setting_screen.dart';
+import 'view/intro/intro_screen.dart';
+import 'view/splash/splash_screen.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -7,9 +22,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Implement state management
     return MaterialApp(
-        // TODO: Add theme data
-        // TODO: Add initial route
-        // TODO: Add routes
-        );
+      theme: kThioAlli,
+      initialRoute: Routes.splash,
+      routes: {
+        Routes.splash: (context) => const SplashScreen(),
+        Routes.intro: (context) => const IntroScreen(),
+        Routes.setting: (context) => const SettingScreen(),
+        Routes.releaseNotes: (context) => const ReleaseNotesScreen(),
+        Routes.theme: (context) => const ThemeScreen(),
+        Routes.about: (context) => const AboutScreen(),
+        Routes.hallOfFame: (context) => const HallOfFameScreen(),
+        Routes.privacyPolicy: (context) => const PrivacyPolicyScreen(),
+        Routes.termsOfService: (context) => const TermsScreen(),
+        Routes.support: (context) => const SupportScreen(),
+        Routes.reportBug: (context) => const ReportBugScreen(),
+      },
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
